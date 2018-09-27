34dc25dd9ab39986bd67165e64ecca608931c91d
Thomas Watson <tpwatson@memphis.edu>
2018-09-27 12:46:52 -0500

    add not tested and kinda broken but working reader

--- camerica_py/vidfile.py
+++ camerica_py/vidfile.py
@@ -151,8 +151,8 @@ class VidfileWriter:
             self.vf_curr_file.flush()
             
             # update the index file too
-            self.index_file.write(struct.pack("<3I", 
-                self.vf_curr_file_num, buf_pos, nbuf))
+            self.index_file.write(struct.pack("<II", 
+                self.vf_curr_file_num, buf_pos))
             self.index_file.flush()
             
             # start a new file if we've gone over this one's size
@@ -182,4 +182,224 @@ class VidfileWriter:
             self.fps,
             self.pack_type,
             self.vf_curr_file_num))
- 
+
+
+class VidfileReader:
+    def __init__(self, path, initial_frame=0):
+        self.path = path
+        
+        self.is_open = True
+        
+        # open the first vidfile so we can extract its metadata
+        self.vf_curr_file = open(self.path, "rb")
+        meta = struct.unpack("<Q5I", self.vf_curr_file.read(8+5*4))
+        self.file_id = meta[0]
+        self.width = meta[1]
+        self.height = meta[2]
+        self.fps = meta[3]
+        self.pack_type = meta[4]
+        self.vf_curr_file_num = meta[5]
+        if self.vf_curr_file_num != 0:
+            raise ValueError("first vidfile should be numbered 0")
+            
+        # attempt to open and read the index
+        self.seek_disabled_reason = None
+        try:
+            indexf = open(self.path+".idx", "rb")
+            index_id, = struct.unpack("<Q", indexf.read(8))
+            if index_id != self.file_id:
+                raise ValueError("index is not for this vidfile")
+            self.seek_table = []
+            while True:
+                record = indexf.read(8)
+                if len(record) == 0:
+                    break
+                if len(record) != 8:
+                    raise ValueError("index is not complete")
+                self.seek_table.append(struct.unpack("<II", record))
+        except Exception as e:
+            self.seek_disabled_reason = str(e)
+            
+        # create queues and events for reader thread
+        self.vf_bufs_to_read = queue.Queue()
+        self.vf_read_bufs = queue.Queue()
+        self.should_be_reading = threading.Event()
+        self.stopped_reading = threading.Event()
+        self.vf_exception = None
+            
+        # allocate read buffers
+        self.vf_curr_buf = None
+        self.vf_curr_bufi = 0
+        self.vf_curr_buf_frames = 0
+        self.vf_curr_frame = 0
+        for x in range(10):
+            # each buffer can hold 1 second of video
+            frame_buf = np.empty(
+                (self.fps, self.height, self.width), dtype=np.uint16)
+            histo_buf = np.empty(
+                (self.fps, 257), dtype=np.uint32)
+            self.vf_bufs_to_read.put((frame_buf, histo_buf))
+         
+        # start reading in separate thread
+        self.vf_reader_thread = threading.Thread(
+            target=self.start_vf_reader)
+        self.vf_reader_thread.start()
+            
+        # and seek to the frame the user requested
+        self.seek(initial_frame)
+        
+    def next_frame(self, frame, histo):
+        if not self.is_open:
+            raise ValueError("attempted to read from closed Vidfile")
+        # get a new buffer, if necessary
+        if self.vf_curr_buf is None:
+            self.vf_curr_buf_frames, self.vf_curr_buf = \
+                self.vf_read_bufs.get()
+            self.vf_curr_bufi = 0
+        
+        # copy from the buffer into the user's
+        fbuf, hbuf = self.vf_curr_buf
+        np.copyto(frame, fbuf[self.vf_curr_bufi, :, :])
+        np.copyto(histo, hbuf[self.vf_curr_bufi, :256])
+        frame_num = hbuf[self.vf_curr_bufi, 256]
+        self.vf_curr_bufi += 1
+        self.vf_curr_frame += 1
+        
+        # queue the buffer for refilling if we've emptied it
+        if self.vf_curr_bufi == self.vf_curr_buf_frames:
+            self.vf_bufs_to_read.put(self.vf_curr_buf)
+            self.vf_curr_buf = None
+            
+        # and return which frame was just read (counting dropped frames)
+        return frame_num
+        
+    def seek(self, frame):
+        if not self.is_open:
+            raise ValueError("attempted to seek on closed Vidfile")
+        if frame == 0:
+            # just rewind back to the beginning
+            self.should_be_reading.clear()
+            #self.vf_bufs_to_read.join()
+            
+            # all the bufs need refilling
+            try:
+                while True:
+                    self.vf_bufs_to_read.put(
+                        self.vf_read_bufs.get(block=False))
+            except queue.Empty:
+                pass
+                
+            # and we need to reopen the file
+            self.vf_curr_file.close()
+            self.vf_open(0)
+            
+            # and let the video reader get back to work
+            self.should_be_reading.set()
+            # read one buffer to make sure playback won't immediately lag
+            self.vf_curr_buf_frames, self.vf_curr_buf = \
+                self.vf_read_bufs.get()
+            self.vf_curr_bufi = 0
+            self.vf_curr_frame = 0
+            
+            return
+            
+        # otherwise, make sure we can actually seek
+        if self.seek_disabled_reason is not None:
+            raise ValueError("Cannot seek: "+self.seek_disabled_reason)
+            
+        # do we actually need to seek?
+        if frame == self.vf_curr_frame: return
+        
+        delta = frame-self.vf_curr_frame
+        
+        # can we seek within the current buffer?
+        if delta+self.vf_curr_bufi < self.vf_curr_buf_frames and \
+                delta+self.vf_curr_bufi >= 0:
+            self.vf_curr_bufi += delta
+            self.vf_curr_frame += delta
+            return
+            
+        # ah well, discard what we have
+        # tell the frame reader to stop as all its work is in vain
+        self.should_be_reading.clear()
+        self.vf_bufs_to_read.join()
+        
+        # all the bufs need refilling
+        try:
+            while True:
+                self.vf_bufs_to_read.put(
+                    self.vf_read_bufs.get(block=False))
+        except queue.Empty:
+            pass
+            
+        # open the correct file
+        fn, fpos = self.seek_table[frame//self.fps]
+        if fn != self.vf_curr_file_num:
+            self.vf_curr_file.close()
+            self.vf_open(fn)
+        self.vf_curr_file.seek(fpos)
+        
+        # and let the video reader get back to work
+        self.should_be_reading.set()
+        # read one buffer to make sure playback won't immediately lag
+        self.vf_curr_buf_frames, self.vf_curr_buf = \
+            self.vf_read_bufs.get()
+        self.vf_curr_bufi = frame % self.fps
+        self.vf_curr_frame = frame
+        
+    def close(self):
+        if self.is_open is False:
+            return
+        self.is_open = False
+        self.vf_bufs_to_read.put(None) # tell thread to exit
+        self.should_be_reading.set()
+        self.vf_reader_thread.join()
+        
+        # delete our buffers to free up memory
+        self.vf_curr_file.close()
+        del self.vf_curr_file
+        del self.vf_bufs_to_read
+        del self.vf_read_bufs
+        
+    def start_vf_reader(self):
+        try:
+            self.vf_reader()
+        except Exception as e:
+            self.vf_exception = e
+            
+    def vf_reader(self):
+        while True:
+            # make sure we're allowed to read
+            self.should_be_reading.wait()
+            
+            bufs = self.vf_bufs_to_read.get()
+            if bufs == None: # asked kindly to exit
+                break
+            fbuf, hbuf = bufs
+            
+            nbufbytes = self.vf_curr_file.read(4)
+            if len(nbufbytes) == 0: # must have hit EOF
+                self.vf_curr_file_num += 1
+                self.vf_open(self.vf_curr_file_num)
+                nbufbytes = self.vf_curr_file.read(4)
+            
+            nbuf, = struct.unpack("<I", nbufbytes)
+            self.vf_curr_file.readinto(fbuf.data)
+            self.vf_curr_file.readinto(hbuf.data)
+            
+            self.vf_read_bufs.put((nbuf, (fbuf, hbuf)))
+            self.vf_bufs_to_read.task_done()
+            
+    def vf_open(self, num):
+        if num == 0:
+            name = self.path
+        else:
+            name = self.path + ".{:04d}".format(num)
+            
+        # open video file and read metadata from it
+        self.vf_curr_file = open(name, "rb")
+        new_file_id, = struct.unpack("<Q", self.vf_curr_file.read(8))
+        if new_file_id != self.file_id:
+            raise ValueError("vidfile {} has incorrect ID".format(name))
+        self.vf_curr_file.seek(8+5*4)
+        self.vf_curr_file_num = num
--- camerica_py/vidshow.py
+++ camerica_py/vidshow.py
@@ -1,21 +1,12 @@
 import pygame
 import numpy as np
 import sys
-
-f = open(sys.argv[1], "rb")
+from vidfile import VidfileReader
 
 palette = [(c, c, c) for c in range(256)]
 
-frsize = (320*256*2)+(256*4)
-
 def get_frame(i):
-    f.seek(i*frsize)
-    frame = np.frombuffer(f.read(320*256*2), dtype=np.uint16)
-    frame.shape = (256, 320)
-
-    histo = np.frombuffer(f.read(256*4), dtype=np.uint32)
-    histo.shape = (256,)
-
+    
     return frame, histo
 
 curr_frame = 10
@@ -24,6 +15,18 @@ disp = pygame.display.set_mode((320*2, 256*2))
 frame_surf = None
 
 clock = pygame.time.Clock()
+
+frame_data = np.empty((256, 320), dtype=np.uint16)
+histo_data = np.empty((256,), dtype=np.uint32)
+
+frame_backing = np.empty((256, 320), dtype=np.uint8)
+
+frame_surf = pygame.image.frombuffer(frame_backing, (320, 256), "P")
+frame_surf.set_palette(palette)
+
+vf = VidfileReader(sys.argv[1])
+
+cf = 0
 
 while True:
     for event in pygame.event.get():
@@ -38,21 +41,17 @@ while True:
                 curr_frame += 1
                 if curr_frame > 2360:
                     curr_frame = 2360
-
-            print(curr_frame)
-            frame_data, histo_data = get_frame(curr_frame)
-            print(histo_data)
-            print(np.bincount(np.reshape(frame_data >> 8, 320*256)))
-            frame_data = (frame_data >> 4).astype(np.uint8)
-            frame_surf = pygame.image.frombuffer(frame_data, (320, 256), "P")
-            frame_surf.set_palette(palette)
-            frame_surf = frame_surf.convert(disp)
-            pygame.image.save(frame_surf, "test.png")
     if event.type == pygame.QUIT:
         break
+        
+    vf.next_frame(frame_data, histo_data)
+    np.copyto(frame_backing, frame_data>>4)
 
-    if frame_surf is not None:
-       pygame.transform.scale(frame_surf, (320*2, 256*2), disp)
+    pygame.transform.scale(frame_surf.convert(disp), (320*2, 256*2), disp)
 
     pygame.display.flip()
     clock.tick(60)
+    cf += 1
+    if cf % 30 == 0:
+        print(clock.get_fps())
+    #print(clock.get_fps())
