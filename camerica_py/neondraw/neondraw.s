fb92bacd5e1cdcf3826c43ef256bc69da7712548
Thomas Watson <tpwatson@memphis.edu>
2018-10-23 12:15:52 -0500

    improve neon efficiency

--- camerica_py/neondraw/neondraw.s
+++ camerica_py/neondraw/neondraw.s
@@ -22,10 +22,10 @@ nd_testadd1:
 // r3 - gain
 // q4-q7 are off limits
 nd_conv_merlin:
-    // load offset into q8 so we can subtract from 8 pixels
-    VDUP.16 q8, r2
-    // and gain into d18 so we can multiply by 4 pixels
-    VDUP.16 d18, r3
+    // load offset into q14 so we can subtract from 8 pixels
+    VDUP.16 q14, r2
+    // and gain into d30 so we can multiply by 4 pixels
+    VDUP.16 d30, r3
     // number of rows
     ldr r2, =256
     // number of bytes per row
@@ -37,45 +37,39 @@ merlin_row:
     // load 8 pixels from source to q0
     VLD1.16 {q0}, [r0]!
     // subtract offset from them and saturate so they don't go below 0
-    VQSUB.U16 q0, q0, q8
+    VQSUB.U16 q1, q0, q14
     // apply scale factor too
-    // widen into q1 and q2
-    VMULL.U16 q1, d0, d18
-    VMULL.U16 q2, d1, d18
+    // widen into q2 and q3
+    VMULL.U16 q2, d2, d30
+    VMULL.U16 q3, d3, d30
     // now shift >> 15 to put intensity in low 8 bits
     // narrow to 16 bits and saturate too
-    VQSHRN.U32 d2, q1, #15
-    VQSHRN.U32 d4, q2, #15
+    VQSHRN.U32 d16, q2, #15
+    VQSHRN.U32 d17, q3, #15
+    // narrow the pixels back down to 8 bits
+    VQMOVN.U16 d24, q8
     // we have to duplicate the pixels horizontally
-    VMOV d3, d2
-    VZIP.16 d3, d2
-    VMOV d5, d4
-    VZIP.16 d5, d4
+    VMOV d25, d24
+    VZIP.8 d24, d25
     
     // now pack and write out groups of 8 pixels
-    // narrow the first group
-    VQMOVN.U16 d20, q1
-    // and duplicate it for the green and blue
-    VMOV d21, d20
-    VMOV d22, d20
+    // first group will be 22, 23, 24, 25
+    VMOV d22, d24
+    VMOV d23, d24
     // we don't care what's in the alpha channel
-    
     // write it on this output row
-    VST4.8 {d20, d21, d22, d23}, [r1]!
+    VST4.8 {d22, d23, d24, d25}, [r1]!
     // and the next one
-    VST4.8 {d20, d21, d22, d23}, [r3]!
+    VST4.8 {d22, d23, d24, d25}, [r3]!
     
     // do the second group in the same way
-    VQMOVN.U16 d20, q2
-    // and duplicate it for the green and blue
-    VMOV d21, d20
-    VMOV d22, d20
-    // we don't care what's in the alpha channel
-    
+    // it will be 25, 26, 27, 28
+    VMOV d26, d25
+    VMOV d27, d25
     // write it on this output row
-    VST4.8 {d20, d21, d22, d23}, [r1]!
+    VST4.8 {d25, d26, d27, d28}, [r1]!
     // and the next one
-    VST4.8 {d20, d21, d22, d23}, [r3]!
+    VST4.8 {d25, d26, d27, d28}, [r3]!
     
     // out of pixels for this row?
     subs r12, r12, #64
