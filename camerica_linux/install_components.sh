df019527b15c2dc08de877f75077387e44f33217
Thomas Watson <twatson52@icloud.com>
2019-03-25 08:01:09 -0500

    add new file to install all the kernel and fpga stuff at once

---
+++ camerica_linux/install_components.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+cd /root/camerica/repo/
+
+echo "MOUNTING BOOT PARTITION"
+mkdir -p /tmp/bootmnt
+mount -t vfat /dev/mmcblk0p1 /tmp/bootmnt
+
+echo "UPDATING KERNEL"
+cp camerica_linux/build/zImage /tmp/bootmnt
+
+echo "UPDATING KERNEL MODULES"
+tar xzf camerica_linux/build/modules.tar.gz -C /
+cp camerica_linux/build/udmabuf.ko ../ko/
+
+echo "UPDATING FPGA CONFIGURATION"
+tar xzf camerica_fpga/sd_fat.tar.gz -C /tmp/bootmnt
+
+echo "UNMOUNTING BOOT PARTITION"
+sync
+umount /tmp/bootmnt
+rm -rf /tmp/bootmnt
+
+echo "COMPONENTS INSTALLED"
+echo "Please reboot the system to load the new components."
